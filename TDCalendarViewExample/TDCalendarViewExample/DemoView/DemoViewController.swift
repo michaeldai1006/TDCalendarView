e955b5de48bb852c29baa1b8f9918505f5b319d1
TDClash <dtc74110@gmail.com>
2018-03-22 11:30:10 -0700

    Organized project

---
+++ TDCalendarViewExample/TDCalendarViewExample/DemoView/DemoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DemoViewController.swift
+//  TDCalendarViewExample
+//
+//  Created by Michael Dai on 3/22/18.
+//  Copyright © 2018 Tiancheng Dai. All rights reserved.
+//
+
+import UIKit
+
+class DemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ TDCalendarViewExample/TDCalendarViewExample/TDCalendarView/TDCalendarView.swift
@@ -0,0 +1,21 @@
+//
+//  TDCalendarView.swift
+//  TDCalendarViewExample
+//
+//  Created by Michael Dai on 3/22/18.
+//  Copyright © 2018 Tiancheng Dai. All rights reserved.
+//
+
+import UIKit
+
+class TDCalendarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
